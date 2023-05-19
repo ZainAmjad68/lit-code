24d2bf2ea6d2640f65ce6768758e65918a80be17
ZainAmjad68 <zainamjad68@yahoo.com>
2023-05-19 12:34:43 +0500

    Valid Anagram (Medium) = 19-05-2023

---
+++ Array/group-anagrams.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        anagram1 = {};
+        anagram2 = {};
+        for letter in s:
+            if letter in anagram1:
+                anagram1[letter] = anagram1[letter] + 1;
+            else:
+                anagram1[letter] = 1;
+
+        for letter in t:
+            if letter in anagram2:
+                anagram2[letter] = anagram2[letter] + 1;
+            else:
+                anagram2[letter] = 1;
+        
+        return anagram1 == anagram2;
+
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        finalList = [];
+        for st in strs:
+            sameLength = [s for s in strs if (len(s) == len(st) and s != st)];
+
+            # if no other element in list is of same length then there's no anagram
+            if not sameLength:
+                finalList.append([st]);
+                continue;
+
+            localList = [st];
+            # if there are anagrams, add them to the final list
+            for sl in sameLength:
+                isAna = self.isAnagram(st, sl);
+                print(isAna);
+                if isAna:
+                    localList.append(sl);
+                    strs.remove(sl);
+                print(localList);
+            finalList.append(localList);
+        return finalList;
+
+solution = Solution();
+strs = ["eat","tea","tan","ate","nat","bat"];
+result = solution.groupAnagrams(strs);
+print(result);
